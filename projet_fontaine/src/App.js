7da4c63101b34abd256f2f7edeec95e498e1be6e
frenger88 <64667571+frenger88@users.noreply.github.com>
2020-12-02 12:52:18 -0500

    Saving progress, added custom icon to map

--- projet_fontaine/src/App.js
+++ projet_fontaine/src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import fountains from "./data/fountains";
+
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+
+import { formatRelative } from "date-fns";
+import mapStyles from "./mapStyles";
 
 function App() {
+  const libraries = ["places"];
+  const mapContainerStyle = {
+    width: "100vw",
+    height: "100vh",
+  };
+  const center = {
+    lat: 45.534811216839316,
+    lng: -73.61851604929831,
+  };
+  const options = {
+    styles: mapStyles,
+    disableDefaultUI: true,
+    zoomControl: true,
+  };
+
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyBgPQJ2VHkedQPu5YMsMgLXz0GDe3qPp30",
+    libraries,
+  });
+
+  if (loadError) return "Error Loading Maps";
+  if (!isLoaded) return "Loading Maps";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <GoogleMap
+        zoom={13}
+        mapContainerStyle={mapContainerStyle}
+        center={center}
+        options={options}
+      >
+        {fountains.map((marker) => (
+          <Marker
+            key={marker._id}
+            position={{ lat: +marker.Latitude, lng: +marker.Longitude }}
+            icon={{
+              url: "/drinkingfountain.svg",
+              scaledSize: new window.google.maps.Size(25, 25),
+              color: "#4287f5",
+            }}
+          />
+        ))}
+      </GoogleMap>
     </div>
   );
 }
